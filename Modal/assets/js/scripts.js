f33ec8cec9239aea1006506f287100692ff1ffd6
renawmontanari <regiangelica@gmail.com>
2022-07-27 16:38:27 -0300

    Modal

---
+++ Modal/assets/js/scripts.js
@@ -0,0 +1,16 @@
+
+'use strict'
+
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
+}
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+});
+
